c836de00603c8807424e69e03ddb87a85ed253b5
elenapulido <119506074+elenapulido@users.noreply.github.com>
2023-05-15 20:26:57 +0200

    mosaic image

--- src/components/Mosaic/Mosaic.jsx
+++ src/components/Mosaic/Mosaic.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import image1 from '../../assets/images/mosaic-8-01.png'
 
 function Mosaic() {
   return (
     <div>
+       <div className="d-flex justify-content-around m-4">
+      <img src={image1} alt="imagen1"style={{  height: "750px" }} />
+      </div>
       
     </div>
   )
