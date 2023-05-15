adf07be92cc11f58ccfab299a6eda89d9966edfe
elenapulido <119506074+elenapulido@users.noreply.github.com>
2023-05-15 18:39:53 +0200

    component commands

--- src/Commands/Commands.jsx
+++ src/Commands/Commands.jsx
@@ -1,12 +1,47 @@
 import React from 'react'
+import image1 from '../assets/images/commands-01.png'
+import image2 from '../assets/images/commands-02.png'
+import image3 from '../assets/images/commands-03.png'
+import image4 from '../assets/images/commands-04.png'
+import image5 from '../assets/images/commands-05.png'
+import image6 from '../assets/images/commands-06.png'
+import image7 from '../assets/images/commands-07.png'
+import image8 from '../assets/images/commands-08.png'
+import image9 from '../assets/images/commands-09.png'
+import image10 from '../assets/images/commands-10.png'
+import image11 from '../assets/images/commands-11.png'
+import image12 from '../assets/images/commands-12.png'
+import image13 from '../assets/images/commands-13.png'
+import image14 from '../assets/images/commands-14.png'
+import image15 from '../assets/images/commands-15.png'
+import image16 from '../assets/images/commands-16.png'
+import image17 from '../assets/images/commands-17.png'
+import image18 from '../assets/images/commands-18.png'
 
 
 
 function Commands() {
   return (
-    <div>
-      
-    </div>
+    <div className="d-flex justify-content-around m-4">
+    <img src={image1} alt="imagen1" style={{ width: "100px", height: "100px" }} />
+    <img src={image2} alt="imagen2" style={{ width: "100px", height: "100px" }} />
+    <img src={image3} alt="imagen3" style={{ width: "100px", height: "100px" }} />
+    <img src={image4} alt="imagen4" style={{ width: "100px", height: "100px" }} />
+    <img src={image5} alt="imagen5" style={{ width: "100px", height: "100px" }} />
+    <img src={image6} alt="imagen6" style={{ width: "100px", height: "100px" }} />
+    <img src={image7} alt="imagen7" style={{ width: "100px", height: "100px" }} />
+    <img src={image8} alt="imagen8" style={{ width: "100px", height: "100px" }} />
+    <img src={image9} alt="imagen9" style={{ width: "100px", height: "100px" }} />
+    <img src={image10} alt="imagen10" style={{ width: "100px", height: "100px" }} />
+    <img src={image11} alt="imagen11" style={{ width: "100px", height: "100px" }} />
+    <img src={image12} alt="imagen12" style={{ width: "100px", height: "100px" }} />
+    <img src={image13} alt="imagen13" style={{ width: "100px", height: "100px" }} />
+    <img src={image14} alt="imagen14" style={{ width: "100px", height: "100px" }} />
+    <img src={image15} alt="imagen15" style={{ width: "100px", height: "100px" }} />
+    <img src={image16} alt="imagen16" style={{ width: "100px", height: "100px" }} />
+    <img src={image17} alt="imagen17" style={{ width: "100px", height: "100px" }} />
+    <img src={image18} alt="imagen18" style={{ width: "100px", height: "100px" }} />
+   </div>
   )
 }
 
