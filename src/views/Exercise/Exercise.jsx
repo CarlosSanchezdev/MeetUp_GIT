f5169fd1157d2c49de03995409e1c8579ce793ad
elenapulido <119506074+elenapulido@users.noreply.github.com>
2023-05-15 20:20:14 +0200

    exercise view

--- src/views/Exercise/Exercise.jsx
+++ src/views/Exercise/Exercise.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import Navbar from '../../components/Navbar/NavBar'
+import Mosaic from '../../components/Mosaic/Mosaic'
 
 function Exercise() {
   return (
     <div>
+        <Navbar />
+        <Mosaic />
       
     </div>
   )
