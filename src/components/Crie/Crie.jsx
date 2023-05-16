fb688bab46057dc086180458a5c63f659c8457c2
elenapulido <119506074+elenapulido@users.noreply.github.com>
2023-05-16 09:02:57 +0200

    mosaic components

---
+++ src/components/Crie/Crie.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Crie() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Crie
---
+++ src/components/Quack/Quack.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Quack() {
+  return (
+    <div>
+     
+     
+    </div>
+  )
+}
+
+export default Quack
