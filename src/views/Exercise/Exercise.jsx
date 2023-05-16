4a87558fd3b3a24d8e507d6ec081a520c3ffc921
Carlos Sánchez <holapueblodev@gmail.com>
2023-05-16 18:33:34 +0200

    finished crie

--- src/views/Exercise/Exercise.jsx
+++ src/views/Exercise/Exercise.jsx
@@ -4,12 +4,14 @@ import Mosaic from '../../components/Mosaic/Mosaic'
 import Baby from '../../components/Baby/Baby'
 import Quack from '../../components/Quack/Quack'
 import Father from '../../components/Father/Father'
+import Crie from '../../components/Cries/Crie'
 
 function Exercise() {
   return (
     <div>
         <Navbar />
         <Father />
+        <Crie />
         <Baby />
         <Quack />
         <Mosaic />
