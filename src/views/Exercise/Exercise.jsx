0470a26c74c538848fca6804fcd2251c0169031f
Carlos Sánchez <holapueblodev@gmail.com>
2023-05-16 11:52:53 +0200

    finish quack

--- src/views/Exercise/Exercise.jsx
+++ src/views/Exercise/Exercise.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import Navbar from '../../components/Navbar/NavBar'
 import Mosaic from '../../components/Mosaic/Mosaic'
+import Quack from '../../components/Quack/Quack'
 
 function Exercise() {
   return (
     <div>
         <Navbar />
+        <Quack />
         <Mosaic />
-      
     </div>
   )
 }
