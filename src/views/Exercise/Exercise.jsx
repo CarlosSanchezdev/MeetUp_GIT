5c2dd20b47ac8dc136019cd23324ad35c3a28384
Carlos Sánchez <holapueblodev@gmail.com>
2023-05-16 18:17:24 +0200

    finished father

--- src/views/Exercise/Exercise.jsx
+++ src/views/Exercise/Exercise.jsx
@@ -3,11 +3,13 @@ import Navbar from '../../components/Navbar/NavBar'
 import Mosaic from '../../components/Mosaic/Mosaic'
 import Baby from '../../components/Baby/Baby'
 import Quack from '../../components/Quack/Quack'
+import Father from '../../components/Father/Father'
 
 function Exercise() {
   return (
     <div>
         <Navbar />
+        <Father />
         <Baby />
         <Quack />
         <Mosaic />
